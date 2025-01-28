71af2f7322df70209661e21d4248e74d6765ffeb
unknown <jomdesaksham2@gmail.com>
2025-01-28 20:13:24 +0530

    Some tests filet

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-     //baseURL: 'https://restful-booker.herokuapp.com/booking',
+     baseURL: 'https://restful-booker.herokuapp.com/booking',
       headless: true, // Always run headless in CI
       screenshot: 'only-on-failure',
       trace: 'retain-on-failure',
