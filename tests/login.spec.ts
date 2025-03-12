d5fe16c5e6d132f721b26b3b425a8e891acb19c7
SakshamJomde11 <jomdesaksham2@gmail.com>
2025-03-12 18:56:50 +0530

    remove test.only test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,19 +9,19 @@ test.describe('Login Tests', () => {
     await loginPage.navigateToLoginPage('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   });
 
-  test.only('Invalid login attempt', async () => {
+  test('Invalid login attempt', async () => {
     await loginPage.performLogin('Admin', 'admin12');
     await expect(loginPage.errorMessage).toBeVisible();
   });
 
-  test.only('Empty field validation', async () => {
+  test('Empty field validation', async () => {
     await loginPage.clickLogin();
     const requiredFields = await loginPage.getRequiredFields();
     await expect(requiredFields.usernameRequired).toBeVisible();
     await expect(requiredFields.passwordRequired).toBeVisible();
   });
 
-  test.only('Successful login and logout', async () => {
+  test('Successful login and logout', async () => {
     await loginPage.performLogin('Admin', 'admin123');
     await expect(loginPage.dashboardHeader).toBeVisible();
     
